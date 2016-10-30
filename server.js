43dafeb8fc489a2fbd7d857da082cc3ad3e7fbfe
Tony Gambone <tony.gambone@deq.virginia.gov>
2016-10-30 12:27:12 -0400

    Persist saved data in the server process.

--- server.js
+++ server.js
@@ -1,28 +1,73 @@
 const
     express = require('express'),
+    bodyParser = require('body-parser'),
     app = express();
 
 app.use(express.static('public'));
-
 app.use('/bootstrap', express.static('node_modules/bootstrap/dist'));
+app.use('/api', bodyParser.json());
+
+const appData = {
+    boards: [
+        { id: 1, title: "Boneyard testing board" }
+    ],
+    lists: [
+        { id: 1, board: 1, name: "List 1" },
+        { id: 2, board: 1, name: "List 2" },
+        { id: 3, board: 1, name: "List 3" }
+    ],
+    cards: [
+        { id: 1, list: 1, title: "Card 1" },
+        { id: 2, list: 1, title: "Card 2" },
+        { id: 3, list: 2, title: "Card 3" },
+        { id: 4, list: 2, title: "Card 4" },
+        { id: 5, list: 2, title: "Card 5" },
+    ]
+};
 
 app.get('/api/boards/:id', (req, res) => {
-    res.json({
-        id: Number(req.params.id),
-        title: "Boneyard testing board",
-        lists: [
-            { id: 2, name: "List 1", cards: [
-                { id: 3, title: "Card 1" },
-                { id: 4, title: "Card 2" }
-            ] },
-            { id: 5, name: "List 2", cards: [
-                { id: 6, title: "Card 3" },
-                { id: 7, title: "Card 4" },
-                { id: 8, title: "Card 5" }
-            ] },
-            { id: 9, name: "List 3", cards: [] },
-        ]
-    });
+    const boardId = Number(req.params.id);
+    const board = appData.boards.filter((b) => b.id === boardId)[0];
+    if (board) {
+        board.lists = appData.lists.filter((l) => l.board === boardId)
+            .map((l) => {
+                l.cards = appData.cards.filter((c) => c.list === l.id);
+                return l;
+            });
+        return res.json(board);
+    } else {
+        res.status(404).json({ error: `Boards item ${boardId} not found` });
+    }
+});
+
+app.put('/api/:collection/:id', (req, res) => {
+    const collection = req.params.collection;
+    const itemId = Number(req.params.id);
+
+    if (!appData.hasOwnProperty(collection)) {
+        res.status(404).json({ error: `Collection ${collection} unknown` });
+        return;
+    }
+
+    const item = appData[collection].filter((x) => x.id === itemId)[0];
+
+    if (!item) {
+        res.status(404).json({ error: `${collection} item ${itemId} unknown` });
+        return;
+    }
+
+    if (collection === 'boards') {
+        console.log('New board title:', req.body.title);
+        item.title = req.body.title;
+    } else if (collection === 'lists') {
+        console.log('New list name:', req.body.name);
+        item.name = req.body.name;
+    } else if (collection === 'cards') {
+        console.log('New card title:', req.body.title);
+        item.title = req.body.title;
+    }
+
+    return res.json(item);
 });
 
 app.listen(3000, function () {
